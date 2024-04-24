0ff8d273bb4beac79b0d5a1f7ca05ae33ad70df1
Samuel Hepworth <samhep0803@gmail.com>
2024-04-24 23:37:52 +0400

    feat: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex h-full flex-col items-center justify-center">
       My links...
     </main>
   );
---
+++ src/components/navbar.tsx
@@ -0,0 +1,91 @@
+import { CircleUser, LinkIcon, Menu } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+
+function Navbar() {
+  return (
+    <header className="bg-background sticky top-0 flex h-16 items-center justify-between gap-4 border-b px-4 md:px-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link
+          href="#"
+          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <LinkIcon className="h-6 w-6" />
+          <div>Sam&apos;s Links</div>
+        </Link>
+        <Link
+          href="#"
+          className="text-foreground hover:text-foreground transition-colors"
+        >
+          Upload
+        </Link>
+        <Link
+          href="#"
+          className="text-muted-foreground hover:text-foreground transition-colors"
+        >
+          Links
+        </Link>
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="#"
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            >
+              <LinkIcon className="h-6 w-6" />
+              <div>Sam&apos;s Links</div>
+            </Link>
+            <Link
+              href="#"
+              className="text-foreground hover:text-foreground transition-colors"
+            >
+              Upload
+            </Link>
+            <Link
+              href="#"
+              className="text-muted-foreground hover:text-foreground transition-colors"
+            >
+              Links
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex items-center gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon" className="rounded-full">
+              <CircleUser className="h-5 w-5" />
+              <span className="sr-only">Toggle user menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
