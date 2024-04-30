e9d23849d136d9c1c4917b22cddaf783e3473d3e
Samuel Hepworth <samhep0803@gmail.com>
2024-04-30 20:46:23 +0400

    feat: update db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,3 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
 import { sql } from "drizzle-orm";
 import {
   index,
@@ -10,25 +7,20 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `links_${name}`);
 
-export const posts = createTable(
-  "post",
+export const links = createTable(
+  "link",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    slug: varchar("slug", { length: 256 }),
+    url: varchar("url", { length: 1024 }),
     createdAt: timestamp("created_at")
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updatedAt"),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (link) => ({
+    slugIndex: index("link_idx").on(link.slug),
+  }),
 );
