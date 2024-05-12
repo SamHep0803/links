ae30327c766fd454627731cfdfa92fdbe136188d
Samuel Hepworth <samhep0803@gmail.com>
2024-05-12 04:03:52 +0400

    Revert "fix: make api endpoint use edge runtime"
    This reverts commit 2d860a2d443780f39f9462145f7bd36976512032.

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,7 +1,5 @@
 import { db } from "@/server/db";
 
-export const runtime = "edge";
-
 export async function GET() {
   const links = await db.query.links.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
