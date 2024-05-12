2d860a2d443780f39f9462145f7bd36976512032
Samuel Hepworth <samhep0803@gmail.com>
2024-05-12 04:00:31 +0400

    fix: make api endpoint use edge runtime

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -1,5 +1,7 @@
 import { db } from "@/server/db";
 
+export const runtime = "edge";
+
 export async function GET() {
   const links = await db.query.links.findMany({
     orderBy: (model, { desc }) => desc(model.createdAt),
